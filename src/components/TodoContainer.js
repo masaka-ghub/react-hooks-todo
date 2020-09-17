22c5390232bb9f5df7596da4a527b0245b7a704e
masaka13 <masaka724.13@gmail.com>
2020-09-17 20:22:54 +0900

    3.useStateを使って入力とTodoリストを管理する

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoContainer = () => {
+  // 入力されたテキストを管理
+  const [input, setInput] = useState('');
+  // Todoリストを管理
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleInput = e => {
+    setInput(e.target.value);
+  };
+
+  const addItem = () => {
+    setTodoItems(prev => [...prev, input]);
+  };
+
   return (
     <>
-      <div className="list-container" />
-      <button onClick={() => alert('Todoを追加する')}>Todo追加</button>
+      <div className="list-container">
+        {todoItems.map((item, i) => (
+          <div key={i}>{item}</div>
+        ))}
+      </div>
+      <input type="text" value={input} onChange={handleInput} />
+      <button onClick={addItem}>Todo追加</button>
     </>
   );
 };
